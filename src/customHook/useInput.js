83a5a97db4c016c0c5066613d9511ff96de6b78a
Sotiria Dialeti <soti.dialeti@gmail.com>
2021-04-19 16:17:25 +0200

    Deploy

--- src/customHook/useInput.js
+++ src/customHook/useInput.js
@@ -1,38 +1,27 @@
-import  {useState} from 'react'
-import noteService from '../services/notes.service';
+import { useState } from "react";
+import noteService from "../services/notes.service";
 
-const useInput1 = () => {    
+const useInput1 = () => {
+  const [values, setValues] = useState({ title: "", content: "" });
 
-    const [values,setValues] = useState({title:"", content:""})
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { title, content } = values;
 
+    noteService.addNote({ title, content }).then(() => {
+      setValues({ title: "", content: "" });
+    });
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const {title, content } = values
+  const handleInputsChange = (event) => {
+    event.persist();
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  return {
+    handleSubmit,
+    handleInputsChange,
+    values,
+  };
+};
 
-        noteService.addNote({title, content })
-        .then(()=> {
-          
-          setValues({title:"",content:""});
-
-        })
-
-        }
-    
-     
-
-    const handleInputsChange = (event) => {
-         event.persist()
-         setValues({...values, [event.target.name]: event.target.value});
-
-      }
-      return {
-        handleSubmit,
-        handleInputsChange,
-        values,
-        
-        
-      };
-}
-
-export default useInput1
+export default useInput1;
