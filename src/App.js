4b8159431bc9be5a0da2fedcb6fd04a5c263e0cb
Sotiria Dialeti <soti.dialeti@gmail.com>
2021-04-19 17:00:18 +0200

    Deploy

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Form from "./components/Notes/Forms/Add";
 
 function App() {
-  
   return (
     <div className="App">
       <Router>
@@ -16,8 +14,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
 
-          <Route exact path="/notes/add" component={Form} />
-          <Route exact path="/notes/edit/:id" />
+          <Route exact path="/add" component={Form} />
+          <Route exact path="/edit/:id" />
         </Switch>
       </Router>
     </div>
